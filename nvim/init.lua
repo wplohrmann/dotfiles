43efe8efac24398cfd79452f1adffebc2a67f096
Will Lohrmann <willia2501@gmail.com>
2026-04-17 11:12:38 +0100

    Fix more things

--- nvim/init.lua
+++ nvim/init.lua
@@ -13,15 +13,6 @@ vim.opt.number = true
 vim.g.clipboard = 'tmux'
 vim.g.mapleader = ","
 
--- space -> Insert mode
-vim.keymap.set('n', '<Space>', 'i')
--- enter -> Save file
-vim.keymap.set('n', '<CR>', ':w<CR>')
--- Move back and forth between buffers
-vim.keymap.set('n', 'l', 'gt')
-vim.keymap.set('n', 'h', 'gT')
--- Find file
-vim.keymap.set('n', '<C-p>', ':Telescope git_files<CR>')
 
 -- LSP
 vim.lsp.config['basedpyright'] = {
@@ -38,12 +29,51 @@ vim.lsp.config['basedpyright'] = {
 }
 vim.lsp.enable('basedpyright')
 
--- Execute last command in the last tab again
-vim.keymap.set('n', '<Leader>r', function()
-    vim.fn.system("tmux send-keys -t ! Up Enter")
-end)
+
+
+require('mini.diff').setup({})
+require('mini.git').setup({})
+require('mini.icons').setup({})
+require('mini.statusline').setup({})
+require('mini.tabline').setup({})
+require('mini.comment').setup({
+    mappings = {
+      -- Toggle comment (like `gcip` - comment inner paragraph) for both
+      -- Normal and Visual modes
+      comment = 'gc',
+
+      -- Toggle comment on current line
+      comment_line = '<C-_>',
+
+      -- Toggle comment on visual selection
+      comment_visual = '<C-_>',
+
+      -- Define 'comment' textobject (like `dgc` - delete whole comment block)
+      -- Works also in Visual mode if mapping differs from `comment_visual`
+      textobject = 'gc',
+    },
+})
 
 vim.keymap.set('n', '<Leader>h', vim.lsp.buf.hover)
 vim.keymap.set('n', '<Leader>r', ':Telescope lsp_references<CR>')
 vim.keymap.set('n', '<Leader>d', ':Telescope lsp_definitions<CR>')
 vim.keymap.set('n', '<Leader>n', vim.lsp.buf.rename)
+vim.keymap.set('n', '<Leader>a', vim.lsp.buf.code_action)
+vim.keymap.set('n', '<Leader>t', vim.lsp.buf.type_definition)
+
+-- space -> Insert mode
+vim.keymap.set('n', '<Space>', 'i')
+-- enter -> Save file
+vim.keymap.set('n', '<CR>', ':w<CR>')
+-- Move back and forth between buffers
+vim.keymap.set('n', 'l', ':bNext<CR>')
+vim.keymap.set('n', 'h', ':bprevious<CR>')
+-- Find file
+vim.keymap.set('n', '<C-p>', ':Telescope git_files<CR>')
+vim.keymap.set('n', '<C-w>', ':bd<CR>')
+vim.keymap.set('n', '<C-f>', ':Telescope live_grep<CR>')
+
+-- Execute last command in the last tab again
+vim.keymap.set('n', '<Leader>r', function()
+    vim.fn.system("tmux send-keys -t ! Up Enter")
+end)
